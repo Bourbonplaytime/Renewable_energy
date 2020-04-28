515724d8594009845b1b8d93f99f3bfb7e1e95dd
Bourbs <Bourbonplaytime@gmail.com>
2020-04-28 08:52:49 -0700

    Continued work on UI

--- UI.py
+++ UI.py
@@ -1,14 +1,26 @@
-from flask import Flask
-from flask import render_template
+import json
+from flask import Flask, render_template, redirect, url_for, request, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
-def view():
-    return render_template("index.html")
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('visuals'))
+    except TypeError:
+        data = {}
+    return data
 
-@app.route('/<name>')
-def index(name='Energy'):
-    return "Renewable {} Statistics".format(name)
+@app.route('/')
+def index():
+    data = get_saved_data()
+    return render_template("index.html", saves=get_saved_data())
+
+@app.route('/show', methods=['POST'])
+def show():
+    response = make_response(redirect(url_for('index')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('visuals', json.dumps(data))
+    return response
 
 app.run(debug=True, port=8000, host='0.0.0.0')
