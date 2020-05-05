e3559b1afaeb4b1c770eb0ea71eff66e54e85b32
Bourbs <Bourbonplaytime@gmail.com>
2020-05-04 17:21:54 -0700

    Introduced by state choropleths into the UI

--- dash_UI.py
+++ dash_UI.py
@@ -22,22 +22,34 @@ sol_results = pd_object('sol', 'solar_electricity')
 hyd_results = pd_object('hyd', 'hydro')
 geo_results = pd_object('geo', 'geothermal')
 
+by_state = pd.read_excel (r'annual_generation_state.xls', header=None)
+by_state = by_state.rename(columns={0:"Year", 1:"State", 2:"Type_of_producer", 3:"Energy_Source", 4:"Generation(Megawatt_Hours)"})
+by_state = by_state.drop([0,1], axis=0)
+
 app = dash.Dash(__name__)
 server = app.server
 
-ren_cats = ['hydro', 'geothermal', 'solar_electricity', 'wind_electricity', 'total_electricity']
+ren_cats = ['hydro', 'geothermal', 'solar_electricity', 'wind_electricity']
+st_cats = ['Hydroelectric Conventional', 'Solar Thermal and Photovoltaic', 'Wind', 'Geothermal']
 app.layout = html.Div([html.H2('US National Renewable Energy by Source 1990-2014'),
     html.Div([dcc.Dropdown(id='category-select', options=[{'label': i.capitalize(), 'value': i} for i in ren_cats],
                            value='hydro', style={'width': '150px'})]),
     dcc.Graph('energy_visual'),
-    html.Div([html.H2('US National Renewable Energy Side by Side by Year in Kilowatt-Hours:Millions 1990-2014'),
+    html.Div([
+    html.H2('US National Renewable Energy Side by Side by Year in Kilowatt-Hours:Millions 1990-2014'),
     dcc.Graph(figure = go.Figure(data=[
     go.Bar(name='Wind', x=wind_results['year'], y=wind_results['quantity']),
     go.Bar(name='Solar', x=sol_results['year'], y=sol_results['quantity']),
     go.Bar(name='Hydro', x=hyd_results['year'], y=hyd_results['quantity']),
     go.Bar(name='Geothermal', x=geo_results['year'], y=geo_results['quantity'])
         ]))
-    ])
+    ]),
+    html.Div([
+    html.H2('US Renewable Energy by State 2018'),
+    dcc.Dropdown(id='source-select', options=[{'label': i.capitalize(), 'value': i} for i in st_cats],
+                           value='Hydroelectric Conventional', style={'width': '150px'}),
+    dcc.Graph('by_state')
+    ]),
 ])
 
 @app.callback(
@@ -62,6 +74,38 @@ def update_graph(grpname):
         return plot
     graph = nat_bar_plotly(op, grpname + '_as_per', grpname)
     return graph
+
+@app.callback(
+    Output('by_state', 'figure'),
+    [Input('source-select', 'value')]
+)
+def make_choropleth(source):
+    def st_df_object(source, year):
+        results = (by_state['Energy_Source'] == source) & (by_state['Type_of_producer'] ==
+                "Total Electric Power Industry") & (by_state['Year'] == year)
+        results = by_state[results]
+        drop = results.index[results['State'] == 'US-Total']
+        results = results.drop(index=drop)
+        return results
+    df = st_df_object(source, 2018)
+    def plotly_choropleth(df, source):
+        fig = go.Figure(data=go.Choropleth(
+        locations=df['State'],
+        z = df['Generation(Megawatt_Hours)'],
+        locationmode = 'USA-states',
+        colorscale = 'Reds',
+        colorbar_title = "Megawatt Hours",
+        ))
+
+        fig.update_layout(
+        title_text = '2018 ' + source + ' Generation by State',
+        geo_scope='usa'
+        )
+
+        return fig
+    st_graph = plotly_choropleth(df, source)
+    return st_graph
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
